50e2c3a9fd0e83f0c8a739d68add46672012b56d
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2021-01-19 19:00:25 +0100

    about us page

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,11 @@ import "../styles/index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="app">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
