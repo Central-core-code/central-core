3a8ae14682be4cd8583312aa4f08fc8d9888b347
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2021-02-01 21:07:14 +0100

    html structure

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Image from "next/image";
 
 function Footer() {
   return (
-    <footer id="footer" name="footer">
-      FOOTER
+    <footer className="container" id="footer" name="footer">
+      <hr />
+      <div className="row justify-content-between">
+        <div class="col-auto">2021 © All rights reserved</div>
+        <div class="col-auto">
+          <span className="mr-2">Privacy</span>
+          <span>Terms</span>
+        </div>
+      </div>
     </footer>
   );
 }
