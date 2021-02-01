294fc239cc5873204968be7a834c41aaa1c96d7c
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2021-02-01 21:27:16 +0100

    form validation

---
+++ components/ContactForm/FormValidation.js
@@ -0,0 +1,25 @@
+const validateName = (name) => {
+  return name.length < 3;
+};
+
+const validateEmail = (email) => {
+  const regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regExp.test(email);
+};
+
+const validateMessage = (msg) => {
+  return msg.length < 3 || msg.length > 200;
+};
+
+export default function formValidation(inputName, value) {
+  switch (inputName) {
+    case "name":
+      return validateName(value);
+    case "email":
+      return validateEmail(value);
+    case "message":
+      return validateMessage(value);
+    default:
+      return false;
+  }
+}
