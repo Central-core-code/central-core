8eb00e6a55c8a0e2f2b376da5dbe2de9976bc8cc
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2021-02-03 00:13:52 +0100

    fix image also for other components

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,22 +1,25 @@
 import React from "react";
-import Image from "next/image";
+import Img from "react-optimized-image";
 import { FooterPrivacy } from "./FooterPrivacy";
 import styles from "@styles/footer.module.scss";
 import Link from "next/link";
+
+import logoBlack from "public/logo_black.svg";
+import envelopeLight from "public/icons/envelope-light.svg";
 
 function Footer() {
   return (
     <footer className={`container ${styles.footer}`} id="footer" name="footer">
       <div className={`row justify-content-between ${styles.footer__block}`}>
         <div className="col-auto">
-          <Image src="/logo_black.svg" width={78} height={41} />
+          <Img src={logoBlack} width={78} height={41} />
         </div>
         <div className="col-auto">
           <Link href="#contactForm">
-            <Image src="/icons/envelope-light.svg" width={30} height={20} />
+            <Img src={envelopeLight} width={30} height={20} />
           </Link>
           <Link href="#contactForm">
-            <Image src="/icons/envelope-light.svg" width={30} height={20} />
+            <Img src={envelopeLight} width={30} height={20} />
           </Link>
         </div>
       </div>
