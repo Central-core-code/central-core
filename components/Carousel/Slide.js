3f8649f168c8b2c6e06b0b79970cae9326900b53
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2021-02-03 23:20:38 +0100

    pierdolony slajder

---
+++ components/Carousel/Slide.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "@styles/slider.module.scss";
+
+function Slide(props) {
+  return <div className={styles.slide}>1</div>;
+}
+
+export default Slide;
