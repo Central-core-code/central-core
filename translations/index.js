e940d49f5509f8c2a7441acfa3c7f0d9f3fa1d00
Konrad Ogielski <konrad.ogielski@infopark.de>
2021-02-04 17:48:55 +0100

    translation

--- translations/index.js
+++ translations/index.js
@@ -10,6 +10,10 @@ export default {
       offer: "Offer",
       contact: "Contact",
     },
+    ourTechnologyBlock: {
+      jsTechnologyDescription:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    },
   },
   pl: {
     helloWorld: "Witaj swiecie",
@@ -21,6 +25,10 @@ export default {
       projects: "Projekty",
       offer: "Oferta",
       contact: "Kontakt",
+    },
+    ourTechnologyBlock: {
+      jsTechnologyDescription:
+        "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do",
     },
   },
 };
