881d12b8e44860bbcaec2d87beff8e39db0e01e8
Konrad Ogielski <konrad.ogielski@infopark.de>
2021-02-08 18:53:15 +0100

    embed

--- components/Carousel/Slide.js
+++ components/Carousel/Slide.js
@@ -6,8 +6,10 @@ import styles from "@styles/slider.module.scss";
 
 function Slide() {
   return (
-    <div className={`${styles.slide} embed-responsive embed-responsive-3by2`}>
-      <Img src={skyTower} />
+    <div className="mx-3">
+      <div className={`${styles.slide} embed-responsive embed-responsive-1by1`}>
+        <Img src={skyTower} className="embed-responsive-item" />
+      </div>
     </div>
   );
 }
