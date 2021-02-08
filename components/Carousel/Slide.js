1b73116248e8e4b4a63abb62b10a975cf75e4fd8
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2021-02-08 20:54:37 +0100

    add alt, compress image, delete unused

--- components/Carousel/Slide.js
+++ components/Carousel/Slide.js
@@ -8,7 +8,11 @@ function Slide() {
   return (
     <div className="mx-3">
       <div className={`${styles.slide} embed-responsive embed-responsive-1by1`}>
-        <Img src={skyTower} className="embed-responsive-item" />
+        <Img
+          src={skyTower}
+          className="embed-responsive-item"
+          alt="our project sky tower book"
+        />
       </div>
     </div>
   );
