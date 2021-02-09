10b26806a2e34a134fd314240872a5ff00d8ecd4
Konrad Ogielski <konrad.ogielski@infopark.de>
2021-02-09 20:26:52 +0100

    style

--- components/ContactForm/index.js
+++ components/ContactForm/index.js
@@ -19,7 +19,7 @@ function ContactForm() {
 
   const locale = getLocale();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
 
     if (!isValidationFailed()) {
@@ -29,32 +29,32 @@ function ContactForm() {
     }
   };
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     const { name, value } = e.target;
 
     const isValidationFailed = formValidation(name, value);
     assignErrors(name, isValidationFailed);
 
-    setForm((prevState) => ({
+    setForm(prevState => ({
       ...prevState,
       [name]: value,
     }));
   };
 
   const isValidationFailed = () => {
-    return Object.keys(errors).some((key) => {
+    return Object.keys(errors).some(key => {
       return errors[key] || form[key] === "";
     });
   };
 
   const assignErrors = (name, validation) => {
-    setErrors((prevState) => ({
+    setErrors(prevState => ({
       ...prevState,
       [name]: validation,
     }));
   };
 
-  const sendEmail = (e) => {
+  const sendEmail = e => {
     e.preventDefault();
 
     emailjs
@@ -65,11 +65,11 @@ function ContactForm() {
         process.env.EMAILJS_USER_KEY
       )
       .then(
-        (result) => {
+        result => {
           setSuccessStatus(true);
           console.log(result.text);
         },
-        (error) => {
+        error => {
           //errors message
           setErrorStatus(true);
           console.log(error.text);
@@ -86,54 +86,78 @@ function ContactForm() {
   }
 
   return (
-    <form
-      className={`${styles.contact_form} py-3`}
-      name="contactForm"
-      id="contactForm"
-      onSubmit={handleSubmit}
-      noValidate
-    >
-      <div
-        className={classNames("mb-3", { [styles.is_error]: errors["email"] })}
-      >
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={form.email}
-          onChange={handleChange}
-          placeholder="e-mail"
-        />
-      </div>
-      <div
-        className={classNames("mb-5 position-relative", {
-          [styles.is_error]: errors["message"],
-        })}
-      >
-        <label htmlFor="message">Message</label>
-        <textarea
-          name="message"
-          id="message"
-          value={form.message}
-          onChange={handleChange}
-          rows="5"
-          placeholder="message"
-        />
-        <small id="emailHelp" className="form-text text-muted float-right">
-          {`${form.message.length}/200`}
-        </small>
-      </div>
-      <div className="text-right">
-        <button
-          className={styles.button}
-          disabled={isValidationFailed()}
-          type="submit"
+    <div id="contactForm" className={styles.contact_wrap}>
+      <div className="container">
+        <div
+          className={`row justify-content-center ${styles.contact_wrap__row}`}
         >
-          {translations[locale].contactForm.sendButton}
-        </button>
+          <div className="col-4">
+            <h3>Let's work together</h3>
+          </div>
+          <div className="col-4">
+            <p>
+              What is the scope of your project? We'll estimate costs and time
+              for you.
+            </p>
+          </div>
+          <div className="col-sm-8">
+            <form
+              className={`${styles.contact_form}`}
+              name="contactForm"
+              onSubmit={handleSubmit}
+              noValidate
+            >
+              <div
+                className={classNames("field position-relative mb-3", {
+                  [styles.is_error]: errors["email"],
+                })}
+              >
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={form.email}
+                  onChange={handleChange}
+                  placeholder="e-mail"
+                />
+                <label htmlFor="email">e-mail</label>
+              </div>
+              <div
+                className={classNames("field mb-5 position-relative", {
+                  [styles.is_error]: errors["message"],
+                })}
+              >
+                <textarea
+                  name="message"
+                  id="message"
+                  value={form.message}
+                  onChange={handleChange}
+                  rows="5"
+                  placeholder="message"
+                />
+                <label htmlFor="message">Message</label>
+                <small
+                  id="emailHelp"
+                  className="form-text text-muted float-right"
+                >
+                  {`${form.message.length}/200`}
+                </small>
+              </div>
+
+              <div className="text-right">
+                <button
+                  className={styles.button}
+                  disabled={isValidationFailed()}
+                  type="submit"
+                >
+                  {translations[locale].contactForm.sendButton}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-    </form>
+    </div>
   );
 }
 
