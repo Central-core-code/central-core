db899f83924b476eb1e63b858fefa0d5de2afc0f
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2021-02-17 18:40:19 +0100

    footer clean up

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import Img from "react-optimized-image";
 import { FooterPrivacy } from "./FooterPrivacy";
 import Link from "next/link";
-import LogoWhite from "public/logo_white.svg";
-import LogoBlack from "public/logo_black.svg";
 import envelopeLight from "public/icons/envelope-light.svg";
 import Logo from "@components/Logo";
 
 function Footer({ bgColor }) {
-  const logo = bgColor === "black" ? LogoWhite : LogoBlack;
-
   return (
     <footer className="py-3 container footer" id="footer" name="footer">
       <div className="row justify-content-between footer__block">
