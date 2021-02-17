35777b4291a350e7579ff1356c3b7ec52582ed37
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2021-02-17 18:39:01 +0100

    handle Logo

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 import LogoWhite from "public/logo_white.svg";
 import LogoBlack from "public/logo_black.svg";
 import envelopeLight from "public/icons/envelope-light.svg";
+import Logo from "@components/Logo";
 
 function Footer({ bgColor }) {
   const logo = bgColor === "black" ? LogoWhite : LogoBlack;
@@ -13,7 +14,7 @@ function Footer({ bgColor }) {
     <footer className="py-3 container footer" id="footer" name="footer">
       <div className="row justify-content-between footer__block">
         <div className="col-auto">
-          <img src={logo} width={78} height={41} alt="logo" />
+          <Logo bgColor={bgColor} />
         </div>
         <div className="col-auto">
           <Link href="#contactForm">
---
+++ components/Logo.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-scroll";
+import { linkProps } from "../utils/linkProps";
+import LogoBlack from "public/logo_black.svg";
+import LogoWhite from "public/logo_white.svg";
+
+function Logo({ bgColor, handleClick = () => {} }) {
+  const logo = bgColor === "black" ? LogoWhite : LogoBlack;
+
+  return (
+    <Link {...linkProps} to="top" onClick={handleClick}>
+      <img
+        src={logo}
+        width={78}
+        height={41}
+        alt="logo"
+        onClick={() => {
+          console.log(1);
+        }}
+      />
+    </Link>
+  );
+}
+
+export default Logo;
