156dda6e0764c5c064a7dc19f86fc3a4799431fd
Konrad Ogielski <konrad.ogielski@infopark.de>
2021-02-23 21:30:56 +0100

    ccc

--- components/Layout.js
+++ components/Layout.js
@@ -30,12 +30,10 @@ function Layout({ children }) {
 
     const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
     tl.fromTo(".intro__logo", { opacity: 0 }, { opacity: 1, duration: 1 });
-    // tl.to(".intro__logo", { opacity: 0, duration: 0.7 });
-    // tl.to(".text", { y: "0%", duration: 0.5, stagger: 0.2 });
-    tl.to(".intro", { opacity: 0, duration: 0.7, delay: 0.5 });
-
-    tl.to(".intro__slider", { y: "-100%", duration: 1, delay: 0.5 });
-    tl.to(".intro", { y: "-100%", duration: 0.5 }, "-=0.5");
+    tl.to(".intro__logo", { opacity: 0, duration:0.5, delay: 0.3 });
+    tl.to(".intro", { opacity: 0, duration:1, delay: 0.3 });
+    tl.to(".intro__slider", { y: "-100%", duration: 0.2 });
+    tl.to(".intro", { y: "-100%", duration: 0.2 }, "-=0.2");
     return () => {
       window.removeEventListener("resize", getSectionsOffset);
       window.removeEventListener("scroll", handleScroll);
@@ -51,12 +49,6 @@ function Layout({ children }) {
           <div className="intro__logo">
             <img className="text" src={logo} alt="Logo" />
           </div>
-          {/* <h1 className="intro__hide">
-            <span className="text">We build</span>
-          </h1>
-          <h1 className="intro__hide">
-            <span className="text">digital world</span>
-          </h1> */}
         </div>
       </div>
       <div className="intro__slider" />
