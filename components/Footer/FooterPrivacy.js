ee9547c9cb5b2d50e97f661ed5935ea817c041aa
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2021-02-23 18:00:48 +0100

    terms and condition

--- components/Footer/FooterPrivacy.js
+++ components/Footer/FooterPrivacy.js
@@ -7,9 +7,9 @@ export function FooterPrivacy() {
       <div className="col-auto">2021 © All rights reserved</div>
       <div className="col-auto">
         <Link href="privacyTerms">
-          <a className="mr-2">Privacy</a>
+          <a className="mr-2">Privacy Terms</a>
         </Link>
-        <Link href="">Terms</Link>
+        <Link href="termsAndConditions">Terms and Condition</Link>
       </div>
     </div>
   );
--- components/Logo.js
+++ components/Logo.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { Link } from "react-scroll";
-import { linkPropsNoActive } from "../utils/linkProps";
 import LogoBlack from "public/logo_black.svg";
 import LogoWhite from "public/logo_white.svg";
+import Link from "next/link";
 
 function Logo({ bgColor, handleClick = () => {} }) {
   const logo = bgColor === "black" ? LogoWhite : LogoBlack;
 
   return (
-    <Link {...linkPropsNoActive} to="top" onClick={handleClick}>
-      <img
-        src={logo}
-        width={78}
-        height={41}
-        alt="logo"
-        className="cursor-pointer"
-      />
+    <Link href="/">
+      <a onClick={handleClick}>
+        <img
+          src={logo}
+          width={78}
+          height={41}
+          alt="logo"
+          className="cursor-pointer"
+        />
+      </a>
     </Link>
   );
 }
