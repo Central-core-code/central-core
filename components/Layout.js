d69e2b1761d7a89330e32a23dc6033df09ddda62
Konrad Ogielski <konrad.ogielski@infopark.de>
2021-02-23 21:10:11 +0100

    speeeed

--- components/Layout.js
+++ components/Layout.js
@@ -31,9 +31,9 @@ function Layout({ children }) {
     const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
     tl.fromTo(".intro__logo", { opacity: 0 }, { opacity: 1, duration: 1 });
     tl.to(".intro__logo", { opacity: 0, duration: 0.7 });
-    tl.to(".text", { y: "0%", duration: 0.7, stagger: 0.2 });
+    tl.to(".text", { y: "0%", duration: 0.5, stagger: 0.2 });
     tl.to(".intro__slider", { y: "-100%", duration: 1, delay: 0.5 });
-    tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+    tl.to(".intro", { y: "-100%", duration: 0.5 }, "-=0.5");
     return () => {
       window.removeEventListener("resize", getSectionsOffset);
       window.removeEventListener("scroll", handleScroll);
