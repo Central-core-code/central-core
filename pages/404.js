326b65628a972cffe9aa50d6d896673a0ec4bb71
Konrad Ogielski <konrad.ogielski@infopark.de>
2021-03-04 15:30:36 +0100

    404

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function NotFound(props) {
   return (
-    <div>
+    <div className="d-flex align-items-center justify-content-center vh-100">
       <h1>404</h1>
     </div>
   );
